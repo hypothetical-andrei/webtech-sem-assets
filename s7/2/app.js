4818d904bc89df4d0323c0a2dff998f8e0bc3036
AT <andrei.toma@ie.ase.ro>
2025-11-19 08:34:32 +0200

    .

---
+++ s7/2/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const Book = require('./Book')
+const app = express()
+const port = 3000
+
+
+const bookRouter = express.Router()
+
+app.use('/api', bookRouter)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+let books = [new Book(1, "Dune", "sf", "Frank Herbert"),
+new Book(2, "Robinson Crusoe", "adventure", "Daniel Defoe"),
+new Book(3, "Foundation", "sf", "Asimov")]
+
+bookRouter.route('/books')
+    //Step 1 - GET request
+    .get((req, res) => {
+        let filteredBooks = [];
+        if (req.query.genre) {
+            filteredBooks = books.filter(x => x.genre === req.query.genre)
+        }
+        else {
+            filteredBooks = books;
+        }
+        res.json(filteredBooks);
+    })
+
+    //Step 2 - POST request
+    .post((req, res) => {
+        let newBook = new Book(req.body.id, req.body.name, req.body.genre, req.body.author);
+
+        books.push(newBook);
+        console.log(books);
+        return res.json(newBook);
+    })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API')
+})
+
+app.listen(port, () => {
+    console.log('Running on the port ' + port)
+})
